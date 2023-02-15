7566fda151db50f9238f3820ed48de44dc09d8e5
Aitana Shough <aeshough@gmail.com>
2023-02-15 10:21:18 -0800

    add passing code for upvote action test
    Co-authored-by: Kirsten Opstad <kirsten.opstad@gmail.com>

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -2,36 +2,36 @@ import * as actions from './../../actions'
 
 describe('readit actions', () => {
 
-  // beforeEach(() => {
+  let dummyState;
 
-  //   const { reviewTitle, reviewAuthor, review, bookAuthor, bookTitle, votes, id, dateTime } = reviewData
-  //   // dummy state data for testing up votes
-  //   dummyState = {
-  //     [1]:
-  //     { 
-  //       reviewTitle: "Cloud Atlas by David Mitchell: A Masterful Blend of Compelling Storytelling and Philosophical Musings",
-  //       reviewAuthor: 'Gloria Patrick Thomas III',
-  //       dateTime: '2023-02-14',
-  //       review: 'Cloud Atlas by David Mitchell is a true masterpiece that left me captivated from beginning to end.',
-  //       votes: 0,
-  //       bookTitle: 'Cloud Atlas',
-  //       bookAuthor: 'David Mitchell',
-  //       id: 1
-  //     },
-  //     [2]:
-  //     { 
-  //       reviewTitle: "Cloud Atlas by David Mitchell: A Masterful Blend of Compelling Storytelling and Philosophical Musings",
-  //       reviewAuthor: 'Floria Patrick Thomas II',
-  //       dateTime: '2023-02-14',
-  //       review: 'Cloud Atlas by David Mitchell is a true masterpiece that left me captivated from beginning to end.',
-  //       votes: 2,
-  //       bookTitle: 'Cloud Atlas',
-  //       bookAuthor: 'David Mitchell',
-  //       id: 2
-  //     }
-  //   }
-  // });
-
+  beforeEach(() => {
+    const { reviewTitle, reviewAuthor, review, bookAuthor, bookTitle, votes, id, dateTime } = reviewData
+    // dummy state data for testing up votes
+    dummyState = {
+      [1]:
+      { 
+        reviewTitle: "Cloud Atlas by David Mitchell: A Masterful Blend of Compelling Storytelling and Philosophical Musings",
+        reviewAuthor: 'Gloria Patrick Thomas III',
+        dateTime: '2023-02-14',
+        review: 'Cloud Atlas by David Mitchell is a true masterpiece that left me captivated from beginning to end.',
+        votes: 0,
+        bookTitle: 'Cloud Atlas',
+        bookAuthor: 'David Mitchell',
+        id: 1
+      },
+      [2]:
+      { 
+        reviewTitle: "Cloud Atlas by David Mitchell: A Masterful Blend of Compelling Storytelling and Philosophical Musings",
+        reviewAuthor: 'Floria Patrick Thomas II',
+        dateTime: '2023-02-14',
+        review: 'Cloud Atlas by David Mitchell is a true masterpiece that left me captivated from beginning to end.',
+        votes: 2,
+        bookTitle: 'Cloud Atlas',
+        bookAuthor: 'David Mitchell',
+        id: 2
+      }
+    }
+  });
   // dummy data for testing
   const reviewData = 
   { 
@@ -62,45 +62,15 @@ describe('readit actions', () => {
 
   it('upvote should create UP_VOTE action', () => {
     expect(actions.upvote(1)).toEqual({
-      reviewTitle: "Cloud Atlas by David Mitchell: A Masterful Blend of Compelling Storytelling and Philosophical Musings",
-      reviewAuthor: 'Gloria Patrick Thomas III',
-      dateTime: '2023-02-14',
-      review: 'Cloud Atlas by David Mitchell is a true masterpiece that left me captivated from beginning to end.',
-      votes: 1,
-      bookTitle: 'Cloud Atlas',
-      bookAuthor: 'David Mitchell',
+      type: 'UP_VOTE',
       id: 1
     })
   })
+
+  
   })
 
 // ADD_POST
 // UP_VOTE
 // DOWN_VOTE
 // SORT_BY_VOTES
-
-    // const { reviewTitle, reviewAuthor, review, bookAuthor, bookTitle, votes, id, dateTime } = reviewData;
-    // action = {
-    //   type: c.ADD_POST,
-    //   reviewTitle: reviewTitle,
-    //   reviewAuthor: reviewAuthor,
-    //   review: review,
-    //   bookTitle: bookTitle,
-    //   bookAuthor: bookAuthor,
-    //   dateTime: dateTime,
-    //   votes: votes,
-    //   id: 1
-    // }
-
-    // expect(postListReducer({}, action)).toEqual({
-    //   [id] : {
-    //     reviewTitle: reviewTitle,
-    //     reviewAuthor: reviewAuthor,
-    //     review: review,
-    //     bookTitle: bookTitle,
-    //     bookAuthor: bookAuthor,
-    //     dateTime: dateTime,
-    //     votes: votes,
-    //     id: 1
-    //   }
-    // })
